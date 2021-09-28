5a3bcc9a6c104295c91e37909c86d348f8af4d57
Valeria Yashan <valeriayashan@gmail.com>
2021-09-28 19:47:55 -0300

    mas config heroku

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ app.get('/', (req,res)=>{
 });
 /* Register */
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + './views/register.html');
 });
 app.post('/register', (req,res)=> {
     res.send('Ya estás registrado');
 });
 /* Login */
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + './views/login.html');
 });
 app.post('/login', (req,res)=>{
     res.send('Ya estás logueado');
 });
 
-app.listen(  process.env.PORT.|| 3000,() => console.log('Servidor corriendo en puerto 3000'));
+app.listen(process.env.PORT||3000,() => console.log('Servidor corriendo en puerto 3000'));
