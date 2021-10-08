1e73383c1767b66f619ed92366c19a0f7bde8ed7
Valeria Yashan <valeriayashan@gmail.com>
2021-10-08 13:25:14 -0300

    vuelta atras

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require ('express');
+const app = express ();
+const path = require ('path');
 
-/* Config express */
-app.use(express.static('public'));
-
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-/* Register */
+    res.sendFile (path.resolve(__dirname,'./views/home.html'));
+})
+
+app.post('/register', (req,res)=>{
+    res.sendFile (path.resolve(__dirname,'./views/home.html'));
+})
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-app.post('/register', (req,res)=> {
-    res.send('Ya te registraste campeón');
-});
-/* Login */
+    res.sendFile (path.resolve(__dirname,'./views/registered.html'));
+})
+
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+    res.sendFile (path.resolve(__dirname,'./views/login.html'));
+})
+
 app.post('/login', (req,res)=>{
-    res.send('Ya te logueaste campeón');
+    res.sendFile (path.resolve(__dirname,'./views/logged.html'));
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
 });
-
-
-
-app.listen(process.env.PORT||3000,() => console.log('Servidor corriendo en puerto 3000'));
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 
 
 
-/*Para actualizar*/
+/*Heroku*/
  heroku create
 $ git push heroku main
 $ heroku open
